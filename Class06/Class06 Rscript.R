fa1671639b0f30247a3ad46baac1a4fa62463fd4
TomQuach317 <92818972+TomQuach317@users.noreply.github.com>
2021-12-05 18:37:49 -0800

    Add files via upload

---
+++ Class06/Class06 Rscript.R
@@ -0,0 +1,9 @@
+#Student 1 
+Student1 <- c(100, 100, 100, 100, 100, 100, 100, 90)
+
+#Student 2
+Studen2 <- c(100, NA, 90, 90, 90, 90, 97, 80)
+
+#Dropping lowest grade of both students
+Student1[-8]
+Student2[
